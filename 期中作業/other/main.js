af2c81b550252801066b17d47557f6fde03f20af
fkmmmgmgg <linxinyi596@gmail.com>
2024-06-14 11:55:48 +0800

    git@github.com:fkmmmgmg/wp.git

---
+++ 期中作業/other/main.js
@@ -0,0 +1,23 @@
+const imageContainerEL= document.querySelector(".image-container");
+const prevEL = document.getElementById("prev");
+const nextEL = document.getElementById("next");
+let x = 0;
+let timer;
+prevEL.addEventListener("click",() => {
+    x = x -45;
+    clearTimeout(timer);
+    updateGallery();
+});
+
+function updateGallery(){
+    imageContainerEL.style.transform ='perspective(1000px) rotateY(${x}deg)';
+    timer = setTimeout(() => {
+        x = x - 45;
+        updateGallery();
+        }
+        ,3000
+);
+
+}
+updateGallery();
+
